2e3782e8b4c48f23799f4d50bf6356f0c0588965
Rafiulsam <rafiulsam69@gmail.com>
2024-05-04 16:12:59 +0600

    main layout added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,9 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../components/Home";
+import Main from "../layout/Main/Main";
 
 const router = createBrowserRouter([
+    // Main Layout
     {
         path:'/',
-        element:<h1>Hello World</h1>
+        element:<Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            }
+        ]
     }
 ])
 
